3473cec55ee0f632b59bce39b8913dce4ef5b19e
Eugene Shevchenko <shevchenko.e.a.ser@gmail.com>
2017-07-04 21:32:55 +0300

    added temporary config file

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,16 @@
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+const {environment} =  require('../config');
+
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger/swagger.json');
 
 module.exports = (app) => {
-    app.use(logger('dev'));
+    if(environment === 'development') {
+        app.use(logger('dev'));
+    }
+
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
         extended: true
