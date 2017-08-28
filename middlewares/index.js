9184f8f2331a2cd9c55e28f953d0c4489e87cd18
Eugene Shevchenko <eugene.shevchenko@swanlogic.com>
2017-08-28 12:30:55 +0300

    added Hystrix metrics middleware (circuit breaker pattern)

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,13 +1,49 @@
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
-const {environment} =  require('../config');
+const {environment} = require('../config');
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger/swagger.json');
 
+const hystrixDashboard = require('hystrix-dashboard');
+const toobusy = require('express-hystrix-toobusy');
+
+// if circuit breaker is open - response 503 http status code
+const hystrixConfig = {
+    fallback: (err, req, res) => {
+        console.log(req);
+        console.log(err.message);
+        if (err.message === 'TooBusy') {
+            res.status(503).end();
+            return;
+        }
+        res.status(500).end();
+    },
+    commandResolver: req => {
+        switch (req.path) {
+            case '/halls':
+                return 'hallsCommand';
+            default:
+                return 'other';
+        }
+    },
+    latencyThreshold: 70,
+    interval: 500,
+    default: {
+        circuitBreakerErrorThresholdPercentage: 50,
+        circuitBreakerRequestVolumeThreshold: 20,
+        circuitBreakerSleepWindowInMilliseconds: 5000
+    },
+    commands: {
+        hallsCommand: {
+            circuitBreakerErrorThresholdPercentage: 50
+        }
+    }
+};
+
 module.exports = (app) => {
-    if(environment === 'development') {
+    if (environment === 'development') {
         app.use(logger('dev'));
     }
 
@@ -16,5 +52,22 @@ module.exports = (app) => {
         extended: true
     }));
 
-    app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+    app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+    // Netfix Hystrix metrics and circuit breaker
+    // for more info about this pattern
+    // @see https://martinfowler.com/bliki/CircuitBreaker.html
+    // for stress test can be usefull JMeter application
+    // for view metrics charts just add url: http://serverIP:serverPort/hystrixMetricsEndpoint/hystrix.stream
+    app.use('/api/metrics', hystrixDashboard({
+        idleTimeout: 500,
+        interval: 500,
+        proxy: true
+    }));
+
+    // use before routes for aggregating metrics info
+    app.use(toobusy(hystrixConfig))
 };
+
+
+
