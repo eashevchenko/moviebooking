3f2123cda9e377d37355c148e5b0251e8991de71
Eugene Shevchenko <eugene.shevchenko@swanlogic.com>
2018-01-18 14:44:57 +0200

    port env variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const PORT = process.env.SERVER_PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
 require('dotenv').config();
 require('./config/db');
